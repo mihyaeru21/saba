1aee4236023749ac108ad47198b745d67b393854
Mihyaeru <mihyaeru21@gmail.com>
2025-12-25 22:26:55 +0900

    chapter 4. p174

--- saba_core/src/renderer/dom/node.rs
+++ saba_core/src/renderer/dom/node.rs
@@ -151,6 +151,7 @@ pub enum ElementKind {
     Style,
     Script,
     Body,
+    P,
 }
 
 impl FromStr for ElementKind {
@@ -163,6 +164,7 @@ impl FromStr for ElementKind {
             "style" => Ok(ElementKind::Style),
             "script" => Ok(ElementKind::Script),
             "body" => Ok(ElementKind::Body),
+            "p" => Ok(ElementKind::P),
             _ => Err(format!("unimplemented element name {:?}", s)),
         }
     }
